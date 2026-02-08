531543fd8c2f667b2c6d14027be70f7931e1456e
I-Maged <magedhanafy@gmail.com>
2026-02-08 08:17:53 +0200

    start react-redux

--- src/Customer.jsx
+++
@@ -1,5 +0,0 @@
-const Customer = () => {
-  return <h2>👋 Welcome, %NAME%</h2>
-}
-
-export default Customer
--- src/store.js
+++ src/store.js
@@ -1,48 +1,13 @@
 // import { createStore } from 'redux'
-import { legacy_createStore as createStore } from 'redux'
+import { combineReducers, legacy_createStore as createStore } from 'redux'
+import accountReducer from './features/accounts/accountSlice'
+import customerReducer from './features/customers/customerReducer'
 
-const initialState = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: '',
-}
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'account/deposit':
-      return { ...state, balance: state.balance + action.payload }
-    case 'account/withdraw':
-      return { ...state, balance: state.balance - action.payload }
-    case 'account/requestLoan':
-      // for later
-      if (state.loan > 0) return state
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      }
-    case 'account/payLoan':
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: '',
-        balance: state.balance - state.loan,
-      }
-
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer)
-
-store.dispatch({ type: 'account/deposit', payload: 543 })
-store.dispatch({ type: 'account/withdraw', payload: 143 })
-store.dispatch({
-  type: 'account/requestLoan',
-  payload: { amount: 5430, purpose: 'college funds' },
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
 })
-store.dispatch({ type: 'account/payLoan' })
 
-console.log(store.getState())
+const store = createStore(rootReducer)
+
+export default store
